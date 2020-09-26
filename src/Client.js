91cfe199dd7ce0496893dcf4119bfa59ed4e7793
unknown <djleonardhd@hotmail.com>
2020-09-26 17:03:26 -0300

    add console function

--- src/Client.js
+++ src/Client.js
@@ -1,11 +1,19 @@
-const { Client: ErisClient } = require("eris");
+const { Client: ErisClient } = require("eris")
+const moment = require('moment')
+const getDate = () => moment.locale('pt-BR') && moment().format('lll')
+const { magenta, red, blue } = require('chalk')
 
 module.exports = class Client extends ErisClient {
     constructor(token, options = {}) {
         super(token, options)
     }
-    
+
     connect () {
         return super.connect()
+    }
+
+    console (error, message, label) {
+        const isError =  error ? red('[Error]') : blue('[Sucess]')
+        console.log(`\x1b[32m[${getDate()}]\x1b[0m${isError} ${message} ${magenta(label)}`);
     }
 }
