5611a6da1ab1d81ba2c0c690bb19893712abe37e
unknown <djleonardhd@hotmail.com>
2020-09-28 13:59:44 -0300

    add embed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const CommandStructures = require('./structures/command')
 
 module.exports = {
-    
+    Embed : require('./structures/Embed'),
     Loader: require('./structures/Loader'),
     Listener: require('./structures/Listener'),
       
     // Commands Structures
     Command: CommandStructures.Command,
+    CommandError: CommandStructures.CommandError,
     CommandContext: CommandStructures.CommandContext,
+    CommandRequirements: CommandStructures.CommandRequirements,
 }
